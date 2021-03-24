8f56e01fea950fcdb3774b3cb4ebd751a8c467cd
alexnikbadin <b.a.d.i.n.alex@yandex.ru>
2021-03-24 12:00:19 +0300

    Lesson_5.1
    Исправленная версия

--- lesson_5.1.sql
+++ lesson_5.1.sql
@@ -17,12 +17,12 @@ CREATE TABLE users (
 ) COMMENT = 'Покупатели';
 
 INSERT INTO users (name, birthday_at, created_at, updated_at) VALUES
-  ('Геннадий', '1990-10-05', now(), now()),
-  ('Наталья', '1984-11-12', now(), now()),
-  ('Александр', '1985-05-20', now(), now()),
-  ('Сергей', '1988-02-14', now(), now()),
-  ('Иван', '1998-01-12', now(), now()),
-  ('Мария', '1992-08-29', now(), now());
+  ('Геннадий', '1990-10-05', DEFAULT, DEFAULT);
+  ('Наталья', '1984-11-12', DEFAULT, DEFAULT),
+  ('Александр', '1985-05-20', DEFAULT, DEFAULT),
+  ('Сергей', '1988-02-14', DEFAULT, DEFAULT),
+  ('Иван', '1998-01-12', DEFAULT, DEFAULT),
+  ('Мария', '1992-08-29', DEFAULT, DEFAULT);
   
  SELECT * FROM users;
 
@@ -31,14 +31,38 @@ INSERT INTO users (name, birthday_at, created_at, updated_at) VALUES
  и в них долгое время помещались значения в формате 20.10.2017 8:10. 
  Необходимо преобразовать поля к типу DATETIME, сохранив введённые ранее значения. */
 
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at VARCHAR(255),
+  updated_at VARCHAR(255)
+) COMMENT = 'Покупатели';
 
- ALTER TABLE users MODIFY created_at varchar(145) NOT NULL;
- ALTER TABLE users MODIFY updated_at varchar(145) NOT NULL;
- DESCRIBE users;
- ALTER TABLE users MODIFY created_at DATETIME DEFAULT CURRENT_TIMESTAMP;
- ALTER TABLE users MODIFY updated_at DATETIME DEFAULT CURRENT_TIMESTAMP;
- SELECT * FROM users;
+INSERT INTO users (name, birthday_at, created_at, updated_at) VALUES
+  -- ('Геннадий', '1990-10-05', '20.10.2017 8:10', '20.10.2017 8:10');
+ ('Геннадий', '1990-10-05', '20.10.2017 8:10', '20.10.2017 8:10');
+  /*('Наталья', '1984-11-12', DEFAULT, DEFAULT),
+  ('Александр', '1985-05-20', DEFAULT, DEFAULT),
+  ('Сергей', '1988-02-14', DEFAULT, DEFAULT),
+  ('Иван', '1998-01-12', DEFAULT, DEFAULT),
+  ('Мария', '1992-08-29', DEFAULT, DEFAULT);*/
 
+SELECT * FROM users;
+DESCRIBE users;
+
+
+ALTER TABLE users ADD COLUMN created_at_new DATETIME;
+UPDATE users SET created_at_new = STR_TO_DATE(created_at, '%m/%d/%Y %H:%i');-- не работает
+ALTER TABLE users DROP COLUMN created_at;
+ALTER TABLE USERS MODIFY created_at_new created_at DATETIME NOT NULL;
+
+
+ALTER TABLE users ADD COLUMN updated_at_new DATETIME;
+UPDATE users SET updated_at_new = STR_TO_DATE(updated_at, '%m/%d/%Y %H:%i');-- не работает
+ALTER TABLE users DROP COLUMN updated_at;
+ALTER TABLE USERS MODIFY updated_at_new updated_at DATETIME NOT NULL;
 
 
 
@@ -60,7 +84,8 @@ CREATE TABLE storehouses_products (
 INSERT INTO storehouses_products (value)
 VALUES (0), (2500), (0), (30), (500), (1);
 SELECT * FROM storehouses_products;
-SELECT value FROM storehouses_products ORDER BY value DESC; --  не могу докрутить до желаемого(
+SELECT value FROM storehouses_products ORDER BY IF (value > 0, 0, 1), value; -- логику не совсем понял
+
 
 
 /* 4. (по желанию) Из таблицы users необходимо извлечь пользователей, 
@@ -71,23 +96,24 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) COMMENT 'Имя покупателя',
-  birthday_at VARCHAR(45) COMMENT 'Дата рождения',
+  birthday_at DATE COMMENT 'Дата рождения',
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) COMMENT = 'Покупатели';
 
 INSERT INTO users (name, birthday_at, created_at, updated_at) VALUES
-  ('Геннадий', '1990 October 05', now(), now()),
-  ('Наталья', '1984 November 12', now(), now()),
-  ('Александр', '1985 May 20', now(), now()),
-  ('Сергей', '1988 February 14', now(), now()),
-  ('Иван', '1998 January 12', now(), now()),
-  ('Мария', '1992 August 29', now(), now());
+  ('Геннадий', '1990-10-05', DEFAULT, DEFAULT),
+  ('Наталья', '1984-11-12', DEFAULT, DEFAULT),
+  ('Александр', '1985-05-20', DEFAULT, DEFAULT),
+  ('Сергей', '1988-02-14', DEFAULT, DEFAULT),
+  ('Иван', '1998-01-12', DEFAULT, DEFAULT),
+  ('Мария', '1992-08-29', DEFAULT, DEFAULT);
   
 
 
-SELECT * FROM users;
-SELECT name, birthday_at FROM users WHERE birthday_at LIKE '%may%' OR birthday_at LIKE '%august%'; 
+SELECT name, MONTHNAME (birthday_at) AS birthday_month FROM users WHERE MONTHNAME (birthday_at) = 'May'
+OR MONTHNAME (birthday_at) = 'August';
+
 
 
 /* 5. (по желанию) Из таблицы catalogs извлекаются записи при помощи запроса. 
@@ -135,19 +161,27 @@ INSERT INTO users (name, birthday_at, created_at, updated_at) VALUES
   ('Сергей', '1988-02-14', now(), now()),
   ('Иван', '1998-01-12', now(), now()),
   ('Мария', '1992-08-29', now(), now());
- 
+SELECT name, ROUND((UNIX_TIMESTAMP(now()) - UNIX_TIMESTAMP (birthday_at))/86400 / 365.25, 1) AS average_age
+FROM users;
+
 SELECT ROUND(AVG(ROUND((UNIX_TIMESTAMP(now()) - UNIX_TIMESTAMP (birthday_at))/86400 / 365.25, 0)), 1) AS average_age
 FROM users;
+
+SELECT (TIMESTAMPDIFF(YEAR, birthday_at, NOW())) AS average_age FROM users; -- этот вариант округляет до 
+-- целых(например Марию он округлил до 28, имея на старте 28.6, что на мой вгляд не совсем корректно)
 
 /* 2. Подсчитайте количество дней рождения, которые приходятся на каждый из дней недели. 
  Следует учесть, что необходимы дни недели текущего года, а не года рождения.*/
 
-SELECT COUNT(*) AS solution, DAYNAME(DATE_FORMAT(birthday_at, '%d.%m.%2021')) AS new_date 
+-- SELECT COUNT(*) AS solution, DAYNAME(DATE_FORMAT(birthday_at, '%d.%m.%2021')) AS new_date 
+-- FROM users GROUP BY new_date;
+
+SELECT COUNT(*) AS solution, DAYNAME(DATE_FORMAT(birthday_at, '%2021-%m-%d')) AS new_date
 FROM users GROUP BY new_date;
 
 
 /* 3. (по желанию) Подсчитайте произведение чисел в столбце таблицы.*/
-
+SELECT * FROM storehouses_products;
 SELECT (EXP (SUM(log(value)))) AS multiple_num FROM storehouses_products; 
 
 
